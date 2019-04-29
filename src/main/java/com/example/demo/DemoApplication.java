2b17eab2fc70c84a92d448575a1464164e0e043c
XiFanYin <807142217@qq.com>
2019-04-29 15:54:23 +0800

    封装Redis工具类

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,11 +3,15 @@ package com.example.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 /*配置数据库文件扫描*/
 @MapperScan("com.example.demo.**.dao")
+/*开启定时任务开关，会自动被springboot2容器管理*/
+@EnableScheduling
 public class DemoApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/example/demo/module/task/TestTask.java
@@ -0,0 +1,22 @@
+package com.example.demo.module.task;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * 定时任务，他会在启动时候自动开启定时任务
+ */
+@Component
+public class TestTask {
+
+    private static final SimpleDateFormat dataFormat = new SimpleDateFormat("HH:mm:ss ");
+    //http://cron.qqe2.com/
+    @Scheduled(cron="4-40 * * * * ? ")
+    public void  reportCurrentTime(){
+        System.out.println("当前时间是"+dataFormat.format(new Date()));
+     }
+
+}
